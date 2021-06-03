a60a2777e6289d6af2d61905836846f86b0c5422
KatherineMcConnell <mcconnell.katherine01@gmail.com>
2021-06-03 10:26:42 -0600

    Added helper method specialties and years hash creation method in person to use in flotilla. Updated test and class. Pryed alot.

--- lib/person.rb
+++ lib/person.rb
@@ -11,4 +11,14 @@ class Person
   def add_specialty(specialty)
     @specialties << specialty
   end
+
+  def specialties_and_yrs_exp
+    yrs_in_spec = {}
+    @specialties.each do |specialty|
+      if yrs_in_spec[specialty].nil?
+        yrs_in_spec[specialty] = @experience
+      end
+    end
+    yrs_in_spec
+  end
 end
