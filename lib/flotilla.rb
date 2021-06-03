0f9fa0f80a2db208d2215b08efe65bf46411fd2c
KatherineMcConnell <mcconnell.katherine01@gmail.com>
2021-06-03 11:26:54 -0600

    Times Up

--- lib/flotilla.rb
+++ lib/flotilla.rb
@@ -11,4 +11,19 @@ class Flotilla
   def add_ship(ship)
     @ships << ship
   end
+
+  def add_personnel(person)
+    @personnel << person
+  end
+
+  def recommend_personnel(ship)
+    reqs = @ships.flat_map do |ship|
+      ship.requirements
+
+    @personnel.each do |person|
+      person.specialties_and_yrs_exp.include?(reqs)
+
+      #still need exp => comparitor
+    end
+  end
 end
